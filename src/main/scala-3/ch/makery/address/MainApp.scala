34d719084d498d41d96b2924c981bf60af637fea
Yong Chun <yongchun_sam@hotmail.com>
2025-06-20 01:56:17 +0800

    Create Application
    - Learn how to create .fxml file with Scene Builder
    - Done Practical

---
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -0,0 +1,39 @@
+package ch.makery.address
+
+import javafx.fxml.FXMLLoader
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes.*
+import javafx.scene as jfxs
+
+object MainApp extends JFXApp3:
+
+  //Window Root Pane
+  var roots: Option[scalafx.scene.layout.BorderPane] = None
+
+  override def start(): Unit =
+    // transform path of RootLayout.fxml to URI for resource location.
+    val rootResource = getClass.getResource("view/RootLayout.fxml")
+    // initialize the loader object.
+    val loader = new FXMLLoader(rootResource)
+    // Load root layout from fxml file.
+    loader.load()
+
+    // retrieve the root component BorderPane from the FXML
+    roots = Option(loader.getRoot[jfxs.layout.BorderPane])
+
+    stage = new PrimaryStage():
+      title = "AddressApp"
+      scene = new Scene():
+        root = roots.get
+
+    // call to display PersonOverview when app start
+    showPersonOverview()
+  // actions for display person overview window
+  def showPersonOverview(): Unit =
+    val resource = getClass.getResource("view/PersonOverview.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load()
+    val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    this.roots.get.center = roots
